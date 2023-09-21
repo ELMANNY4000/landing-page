25192bcbc41ce6a8db50c5c862fa30e5b381f97d
Emmanuel <ulodoe@gmail.com>
2023-09-21 20:24:48 +0100

    header

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="first"></div>
+        <div className="second"></div>
+        <div className="third"></div>
+    </nav>
+  )
+}
+
+export default Navbar
